f587d7e3e8356d490fb35426bd57e403e784fdfe
Rowe Wilson Frederisk Holme <frederisk@outlook.com>
2024-03-08 00:57:11 +0800

    Add custom scripts support

---
+++ utils/js/MathJaxAmChtml.js
@@ -0,0 +1,7 @@
+(function(){
+    var MathJaxUrl = 'https://cdnjs.cloudflare.com/ajax/libs/mathjax/2.7.1/MathJax.js?config=TeX-MML-AM_CHTML';
+    var script = document.createElement("script");
+    script.type = "text/javascript";
+    script.src = MathJaxUrl;
+    document.head.appendChild(script);
+})();
